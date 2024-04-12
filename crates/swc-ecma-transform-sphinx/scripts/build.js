78541a4ac611d69ef3786677485d8bed96570586
Tony Wu <tonywu6@protonmail.com>
2024-04-12 08:28:51 +0800

    chore: deno

--- crates/swc-ecma-transform-sphinx/scripts/build.js
+++ crates/swc-ecma-transform-sphinx/scripts/build.js
@@ -1,10 +1,16 @@
 import { fileURLToPath } from "node:url";
 
 import * as esbuild from "esbuild";
+import { denoPlugins } from "jsr:@luca/esbuild-deno-loader@^0.10.3";
 
 const relpath = (path) => fileURLToPath(new URL(path, import.meta.url));
 
 await esbuild.build({
+  plugins: [
+    ...denoPlugins({
+      configPath: relpath("../deno.json"),
+    }),
+  ],
   entryPoints: [relpath("../src/js/server/index.ts")],
   outfile: relpath("../dist/server/index.js"),
   format: "esm",
